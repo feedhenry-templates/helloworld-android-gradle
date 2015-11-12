cc4ff3ca7d190b6d9bb600af7995738f95369fab
Summers Pittman <secondsun@gmail.com>
2015-11-12 15:14:12 -0500

    Adding in code coverage

---
+++ app/src/androidTest/java/com/feedhenry/helloworld/test/JUnitJacocoTestRunner.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2015 FeedHenry Ltd, All Rights Reserved.
+ *
+ * Please refer to your contract with FeedHenry for the software license agreement.
+ * If you do not have a contract, you do not have a license to use this software.
+ */
+package com.feedhenry.helloworld.test;
+
+import android.os.Bundle;
+import android.support.test.runner.AndroidJUnitRunner;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Provides a workaround for https://code.google.com/p/android/issues/detail?id=170607
+ * Will be removed in android test-runner version 0.4
+ */
+public class JUnitJacocoTestRunner extends AndroidJUnitRunner {
+    static {
+        System.setProperty("jacoco-agent.destfile", "/data/data/com.feedhenry.helloworld/coverage.ec");
+    }
+
+    @Override
+    public void finish(int resultCode, Bundle results) {
+        try {
+            Class rt = Class.forName("org.jacoco.agent.rt.RT");
+            Method getAgent = rt.getMethod("getAgent");
+            Method dump = getAgent.getReturnType().getMethod("dump", boolean.class);
+            Object agent = getAgent.invoke(null);
+            dump.invoke(agent, false);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        super.finish(resultCode, results);
+    }
+}
