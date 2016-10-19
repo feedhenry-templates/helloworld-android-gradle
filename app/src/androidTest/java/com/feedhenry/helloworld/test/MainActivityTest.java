d4b0541552e1f497ccede66f59235a8f79229474
Daniel Passos <daniel@passos.me>
2016-10-19 19:41:58 -0200

    Move tests to the new Android Instrument test style

--- app/src/androidTest/java/com/feedhenry/helloworld/test/MainActivityTest.java
+++ app/src/androidTest/java/com/feedhenry/helloworld/test/MainActivityTest.java
@@ -15,22 +15,15 @@
  */
 package com.feedhenry.helloworld.test;
 
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.app.Fragment;
-import android.test.ActivityUnitTestCase;
-import android.util.Log;
-import android.view.ContextThemeWrapper;
 
 import com.feedhenry.helloworld.HelloFragment;
 import com.feedhenry.helloworld.MainActivity;
 import com.feedhenry.helloworld_android.R;
-import com.squareup.okhttp.mockwebserver.MockWebServer;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -38,51 +31,46 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
 
-public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
 
-
-    private MockWebServer mockWebServer = null;
     private long startTime;
-    public MainActivityTest() {
-        super(MainActivity.class);
-    }
 
+    private MainActivity mMainActivity;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
-        mockWebServer = new MockWebServer();
-        mockWebServer.start(9000);
-        ContextThemeWrapper context = new AlternateAssetsContextWrapper(getInstrumentation().getTargetContext(), R.style.MyTheme_Base, getInstrumentation().getContext());
-        setActivityContext(context);
+        mMainActivity = mActivityRule.getActivity();
         startTime = System.currentTimeMillis();
     }
 
+    @After
+    public void tearDown() throws Exception {
+        mMainActivity.finish();
+    }
 
+    @Test
+    public void testPreconditions() {
+        assertNotNull(mMainActivity);
+    }
+
+    @Test
     public void testActivityCallsFHInitOnStartup() throws IOException {
-        getInstrumentation().runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                MainActivity activity = startActivity(new Intent(), Bundle.EMPTY, null);
-                activity.onStart();
-            }
-        });
-
-
-        MainActivity main = getActivity();
-
-
         Fragment f;
-        while (!((f = main.getSupportFragmentManager().findFragmentById(R.id.content)) instanceof HelloFragment)) {
+        while (!((f = mMainActivity.getSupportFragmentManager()
+                .findFragmentById(R.id.content)) instanceof HelloFragment)) {
             assertTrue("Timeout after 10 seconds", System.currentTimeMillis() - startTime < 10000);
         }
 
         Assert.assertEquals(HelloFragment.class, f.getClass());
-
-        main.finish();
-
     }
 
 }
